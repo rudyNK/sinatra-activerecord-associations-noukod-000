74b4b79884807bb2167e4a5a01a9175bcba6c70e
Rudy Ravelin <rudy@noukod.com>
2019-01-17 19:09:30 +0000

    Automatically backed up by Learn

--- db/migrate/20150915202628_create_cats.rb
+++ db/migrate/20150915202628_create_cats.rb
@@ -1,9 +1,9 @@
 class CreateCats < ActiveRecord::Migration
   def change
-      create_table :cats do |t|
-        t.string :name
-        t.integer :age
-        t.string :breed
-      end
+    create_table :cats do |t|
+      t.string :name
+      t.integer :age
+      t.string :breed
     end
   end
+end
