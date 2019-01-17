73bdb620dae4dd83242402b5ac9e1964f2484534
Rudy Ravelin <rudy@noukod.com>
2019-01-17 19:14:28 +0000

    Automatically backed up by Learn

--- db/migrate/20150915202821_add_column_to_cats.rb
+++ db/migrate/20150915202821_add_column_to_cats.rb
@@ -1,9 +1,14 @@
+# class AddColumnToCats < ActiveRecord::Migration
+#   def up
+#     add_column :cats, :owner_id, :integer
+#   end
+# 
+#   def down
+#     remove_column :cats, :owner_id
+#   end
+# end
 class AddColumnToCats < ActiveRecord::Migration
-  def up
+  def change
     add_column :cats, :owner_id, :integer
-  end
-
-  def down
-    remove_column :cats, :owner_id
   end
 end
